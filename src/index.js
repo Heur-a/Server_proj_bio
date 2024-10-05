83dfb251ddc4e38ae7e6095aa5a6c638d4e67e7c
heura <frogadier100@gmail.com>
2024-10-05 20:07:16 +0200

    Funciona base datos minimament

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-import express from 'express'
+import express from 'express' 
+import { createPool } from 'mysql2/promise'
+import second from 'dotenv/config'
 
 const app = express()
+
+console.log(
+    {
+        host: process.env.MYSQLDB_HOST,
+        user: process.env.MYSQLDB_USER,
+        password: process.env.MYSQLDB_PASSWORD,
+        port: process.env.MYSQLDB_PORT
+    }
+)
+
+
+const pool = createPool({
+    host: process.env.MYSQLDB_HOST,
+    user: process.env.MYSQLDB_USER,
+    password: process.env.MYSQLDB_PASSWORD,
+    port: 3306
+})
 
 app.get('/', (req, res) => 
     {
@@ -8,5 +27,13 @@ app.get('/', (req, res) =>
     }
 )
 
-app.listen(3000)
+app.get('/ping', async (req, res) =>
+    {
+        const result = await pool.query('SELECT NOW()')
+        res.json(result[0])
+    }
+)
+
+
+app.listen(80)
 console.log('Server listening on port 3000')
