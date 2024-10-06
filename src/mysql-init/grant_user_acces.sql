21809130c762c060fd394c81873b182c98233a5c
heura <frogadier100@gmail.com>
2024-10-06 18:13:52 +0200

    hotfix sql

--- src/mysql-init/grant_user_acces.sql
+++ src/mysql-init/grant_user_acces.sql
@@ -7,11 +7,10 @@
 
 
 
-----------------------------------------------------------------------------------------
 -- This is an optional script to grant access to the server user to the server database
 -- This script is executed by the server container when the server database is created
--- If you want to use this script, you must change the user name on this script
-----------------------------------------------------------------------------------------
+-- If you want to use this script, you must change the user name on this Script
+
 GRANT SELECT, INSERT, UPDATE ON *.* TO 'server'@'%';
 
 -- Actualitzar els privilegis per assegurar-se que tenen efecte immediatament
