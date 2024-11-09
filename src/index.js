29ca253f9a89d95c9d9718fc70f6d01b39fed1db
heura <frogadier100@gmail.com>
2024-11-09 13:01:22 +0100

    Mirar si correo en uso registro

--- src/index.js
+++ src/index.js
@@ -1,167 +1,131 @@
-/*
- * @Author: Alex Escrivà Caravaca 
- * @Date: 2024-10-09 10:23:28 
- * @Last Modified by: Alex Escrivà Caravaca
- * @Last Modified time: 2024-10-27 19:22:45
- */
-/**
- * @file index
- * @brief Entry point of the API application that initializes the Express server, sets up routes, and serves API documentation.
- * @requires express
- * @requires path
- * @requires fileURLToPath
- * @requires medicionesRoutes
- * @requires swaggerUi
- * @requires YAML
- * @requires config
- * @description This file configures and starts the Express server, connects to the database, sets up middleware, and loads API documentation using Swagger UI.
- * 
- * This file configures and starts the Express server, connects to the database, sets up middleware, and loads API documentation using Swagger UI.
- * It defines the main route and handles requests to other routes through dedicated route modules.
- * 
- * @author Alex Escrivà Caravaca
- * @date 2024-10-06
- * @version 1.0.0
- * @last_modified 2024-10-09
- */
-
 import express from 'express';
-import path from 'path';  // To resolve file paths
-import { fileURLToPath } from 'url';  // To work with ES modules
-import medicionesRoutes from './routes/medicionesRoutes.js';  // Route handling for measurement endpoints
-import usersRoutes from './routes/usersRoutes.js';  // Route handling for user endpoints
-import authRoutes from './routes/authRoutes.js';  // Route handling for authentication endpoints
-import nodeRoutes from './routes/nodeRoutes.js'; // Route handling for node endpoints
-import swaggerUi from 'swagger-ui-express';  // Swagger UI for API documentation
-import YAML from 'yamljs';  // To load the API documentation from a YAML file
-import { config } from 'dotenv';  // To load environment variables from a .env file
+import path from 'path';
+import { fileURLToPath } from 'url';
+import medicionesRoutes from './routes/medicionesRoutes.js';
+import usersRoutes from './routes/usersRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import nodeRoutes from './routes/nodeRoutes.js';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import { config } from 'dotenv';
 import https from 'https';
 import fs from 'fs';
-import { sessionMiddleware, verifyIdentity } from './services/authService.js'; // Import session middleware
+import { sessionMiddleware, verifyIdentity } from './services/authService.js';
 
-// Initialize the Express app
+/**
+ * @file index.js
+ * @module server
+ * @description Entry point of the API application that initializes the Express server, sets up routes, and serves API documentation.
+ * This file configures and starts the Express server, connects to the database, sets up middleware, and loads API documentation using Swagger UI.
+ */
+
+/**
+ * Initializes the Express application and middleware.
+ * Loads environment variables, sets up session management, and configures JSON parsing.
+ */
 const app = express();
-
-// Load environment variables from .env file
 config();
 
-// Load Swagger API documentation from YAML file
+/**
+ * Loads the Swagger API documentation from a YAML file.
+ * @constant {Object} swaggerDocument - The loaded Swagger documentation object.
+ */
 const swaggerDocument = YAML.load('./doc/api/api.yaml');
 
-// Configure Express to use sessions with MemoryStore (default)
-app.use(sessionMiddleware); // Use session middleware from authService
+/**
+ * Middleware for session management.
+ * @function sessionMiddleware
+ */
+app.use(sessionMiddleware);
 
-// URL to fetch the latest measurement
-const url = 'http://localhost/mediciones/ultima';
-
-// Middleware for parsing JSON requests
+/**
+ * Middleware for parsing JSON requests.
+ * @function express.json
+ */
 app.use(express.json());
 
-// Get the current directory (ES modules replacement for __dirname)
+/**
+ * Retrieves the current file's directory name.
+ * @constant {string} __filename - The current file's URL.
+ * @constant {string} __dirname - The directory name of the current module.
+ */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Configure https server
+/**
+ * Creates an HTTPS server with SSL certificates.
+ * @constant {https.Server} server - The HTTPS server instance.
+ */
 const server = https.createServer({
     key: fs.readFileSync(path.join(__dirname, './certs/fake-key.pem'), 'utf8'),
     cert: fs.readFileSync(path.join(__dirname, './certs/fake-cert.pem'), 'utf8'),
 }, app);
 
 /**
- * @brief Serves the Swagger API documentation.
- * 
- * This middleware serves the API documentation generated from the `api.yaml` file in the `/api-docs` path.
- * It provides an interactive interface for testing the API's endpoints.
- * 
+ * Sets up the Swagger API documentation route.
  * @route /api-docs
  */
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 /**
- * @brief Sets up the routes for measurement (mediciones) endpoints.
- * 
- * This middleware uses the `medicionesRoutes` module to handle all requests to `/mediciones`.
- * The actual logic for these routes is managed in the corresponding controller.
- * 
+ * Sets up the routes for measurement endpoints.
  * @route /mediciones
  */
 app.use('/mediciones', medicionesRoutes);
 
 /**
- * @brief Serves up the routes for users endpoints.
- * 
- * This middleware uses the `usersRoutes` module to handle all requests to `/users`.
- * The actual logic for these routes is managed in the corresponding controller.
- * 
+ * Sets up the routes for user endpoints.
  * @route /users
- * @see usersRoutes
  */
 app.use('/users', usersRoutes);
 
 /**
- * @brief Auth API route
- *
- * This middleware uses the authRoutes module to handle all requests to /auth.
- * The actual logic for these routes is managed in the corresponding controller.
- *
+ * Sets up the routes for authentication endpoints.
  * @route /auth
- * @see authRoutes
  */
 app.use('/auth', authRoutes);
 
 /**
-*@brief Node API route
-*
-*This middleware uses the nodeRoutes module to handle all requests to /node.
-*The actual logic for these routes is managed in the corresponding controller.
-*
-*@route /node
-*@see nodeRoutes
-*/
+ * Sets up the routes for node endpoints.
+ * @route /node
+ */
 app.use('/node', nodeRoutes);
 
 /**
- * @brief User route for the web server with authentication check.
- *
- * This middleware serves any static files (HTML, CSS, JS, images) located in the `public/user` directory.
- * It requires user authentication to access this route.   
+ * Serves static files for authenticated users from the public/user directory.
+ * @route /user
  */
 app.use('/user', verifyIdentity, express.static(path.join(__dirname, 'public/user')));
 
 /**
- * @brief Serves static HTML files from the "public" directory.
- * 
- * This middleware serves any static files (HTML, CSS, JS, images) located in the `public` directory.
- * For example, requests to `/index.html` will serve `public/index.html`.
+ * Serves static files from the public directory.
+ * @route /
  */
 app.use(express.static(path.join(__dirname, 'public')));
 
 /**
- * @brief Main route for the web server.
- * 
- * The root route `/` serves the `index.html` file located in the `public` directory.
- * 
+ * Main route for serving the index.html file.
+ * @route /
  * @param {Request} req - The HTTP request object.
  * @param {Response} res - The HTTP response object.
- * 
- * @route /
  */
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 /**
- * @brief Starts the Express server and listens on a specific port.
- * 
- * The server listens on the port specified in the environment variables (`PORT`) or defaults to port 3000.
- * When the server starts, it logs the MySQL database connection details from the environment variables.
+ * Starts the HTTPS server and listens on port 443.
+ * Logs the server status and database connection details.
  */
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server running on port ${process.env.PORT || 3000}`);
-    console.log(`Connected to MySQL database at ${process.env.DB_HOST}:${process.env.MYSQLDB_PORT}`);
-});
-
 server.listen(443, () => {
     console.log('Server running on port 443');
     console.log(`Connected to MySQL database at ${process.env.DB_HOST}:${process.env.MYSQLDB_PORT}`);
+});
+
+/**
+* Starts the HTTP server and listens on port 80.
+* Redirects all HTTP requests to the HTTPS server.
+*/
+app.listen(80, () => {
+    console.log('Server running on port 80');
 });
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -2,17 +2,19 @@
  * @Author: Alex Escrivà Caravaca 
  * @Date: 2024-10-21 12:57:37 
  * @Last Modified by: Alex Escrivà Caravaca
- * @Last Modified time: 2024-10-23 19:56:27
+ * @Last Modified time: 2024-11-09 12:36:28
  */
 
 /**
  * @module medicionesService
- * @description Routing for the users api
+ * @description Routing for the users API.
+ * This module defines the routes for user-related operations, including fetching,
+ * creating, updating, and deleting users. It utilizes Express Router to handle
+ * incoming requests and delegates the logic to the appropriate controller functions.
  */
 
 import { Router } from 'express';  // Import the Router from Express
-import { createUser, deleteUser, getUserByEmail, updateUser } from '../controllers/userController.js';
-
+import { checkEmailInUse, createUser, deleteUser, getUserByEmail, updateUser } from '../controllers/userController.js';
 // Initialize the router
 const router = Router();
 
@@ -20,44 +22,56 @@ const router = Router();
  * @brief Route for fetching all users.
  * 
  * This route handles GET requests to the `/users` endpoint. It invokes the
- * `getUsers` controller function to retrieve all users from the database.
+ * `getUserByEmail` controller function to retrieve all users from the database.
  * 
  * @route GET /users
- * @see getUsers
+ * @see getUserByEmail
  */
 router.get('/', getUserByEmail);
 
 /**
  * @brief Route for posting a new user.
- *  This route handles POST requests to the `/users` endpoint. It invokes the
- * `postUser` controller function to add a new user to the database.
- *  @route POST /users
- * @see postUser
+ * 
+ * This route handles POST requests to the `/users` endpoint. It invokes the
+ * `createUser` controller function to add a new user to the database.
+ * 
+ * @route POST /users
+ * @see createUser
  */
 router.post('/', createUser);
 
-
-
-
 /**
  * @brief Route for deleting a user by id.
+ * 
  * This route handles DELETE requests to the `/users/:id` endpoint. It invokes the
- * `deleteUserById` controller function to delete a user from the database.
+ * `deleteUser` controller function to delete a user from the database.
+ * 
  * @route DELETE /users/:id
- * @see deleteUserById
+ * @see deleteUser
  */
-
-router.delete('/', deleteUser);
+router.delete('/:id', deleteUser);
 
 /**
  * @brief Route for updating a user by id.
+ * 
  * This route handles PUT requests to the `/users/:id` endpoint. It invokes the
- * `updateUser` controller function to update a user from the database.
+ * `updateUser` controller function to update a user in the database.
+ * 
  * @route PUT /users/:id
  * @see updateUser
  */
+router.put('/:id', updateUser);
 
-router.put('/', updateUser);
+/**
+ * @brief Route for checking if an email is already in use.
+ * 
+ * This route handles GET requests to the `/users/checkemail` endpoint. It invokes the
+ * `checkUserExists` controller function to check if the provided email exists in the database.
+ * 
+ * @route GET /users/checkemail
+ * @see checkEmailInUse
+ */
+router.get('/checkemail', checkEmailInUse);
 
 // Export the router to be used in other modules
 export default router;
